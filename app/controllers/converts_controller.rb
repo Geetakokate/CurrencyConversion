b01ad4a6ed15c58252384484f6ccc7247c8a19a3
imAkshay26 <akshay@joshsoftware.com>
2018-09-25 13:52:21 +0530

    view generated

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -1,14 +1,16 @@
 class ConvertsController < ApplicationController
+	require 'net/http'
 
   def exchange
     
   end
 
   def create    
-    response = Net::HTTP.get(URI("https://api.exchangeratesapi.io/latest?base=#{params[:currency_code]}"))
+    response = Net::HTTP.get(URI("https://api.exchangeratesapi.io/latest?base=#{params[:converts][:currency_code]}"))
     result = JSON.parse(response)
-    rate = result['rates'][params[:currency_code_to_convert]]
-    converted_amount = rate * params[:amount].to_i
+    rate = result['rates'][params[:converts][:currency_code_to_convert]]
+    @converted_amount = rate * params[:converts][:amount].to_i
+    render 'exchange_result.js'
   end
 
 end
